9e870f1c1d97274c4d467e8b1af837b726edc4ed
privateOmega <privateOmega@protonmail.com>
2022-03-14 15:01:18 +0530

    Setup pyupdater

---
+++ client_config.py
@@ -0,0 +1,7 @@
+class ClientConfig(object):
+    PUBLIC_KEY = 'None'
+    APP_NAME = 'PyUpdater Trial'
+    COMPANY_NAME = 'PrivateOmega'
+    HTTP_TIMEOUT = 30
+    MAX_DOWNLOAD_RETRIES = 3
+    UPDATE_URLS = ['']
